0b62213b9cd67dc1186187382b8aea0e3bf22cbf
Daniella <da.vegam@duocuc.cl>
2025-07-13 16:10:16 -0400

    Nuevo integrante

--- integrantes.py
+++ integrantes.py
@@ -1,4 +1,4 @@
-integrantes = ["Cristopher Sepulveda", "Daniella Vega"]
+integrantes = ["Cristopher Sepulveda", "Daniella Vega", "Marcelo Menares"]
 print ("Integrantes del grupo:")
 for i in integrantes:
     print("_",i)
